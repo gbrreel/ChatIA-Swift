260cf390f36ae1c5804a4fd0913a4a1ead9b456b
gbrreel <gabrielggalloo@gmail.com>
2025-02-05 20:35:12 -0300

    Inicio projeto

---
+++ chatIA/chatIA/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  chatIA
+//
+//  Created by Gabriel Gallo on 02/02/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ chatIA/chatIATests/chatIATests.swift
@@ -0,0 +1,17 @@
+//
+//  chatIATests.swift
+//  chatIATests
+//
+//  Created by Gabriel Gallo on 02/02/25.
+//
+
+import Testing
+@testable import chatIA
+
+struct chatIATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
